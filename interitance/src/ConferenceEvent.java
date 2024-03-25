6ed39e7c36210061845c67b60d58b4d679b32521
GenshinmasterJinHang <161272990+GenshinmasterJinHang@users.noreply.github.com>
2024-03-25 16:49:32 +0800

    Add files via upload

--- interitance/src/ConferenceEvent.java
+++ interitance/src/ConferenceEvent.java
@@ -1,121 +1,58 @@
-public class ConferenceEvent extends Event{
+public class ConferenceEvent extends Event implements
+CalculateEventCostInterface{
     private boolean breakfastRequired;
-    private boolean lunchRequired;
-    private boolean dinnerRequired;
     private double breakfastCost;
+    private boolean lunchRequired;
     private double lunchCost;
+    private boolean dinnerRequired;
     private double dinnerCost;
-    private double ConferenceEventcost;
-   
-    public ConferenceEvent (){    
-    }
-    public ConferenceEvent(String eventID, String eventName, String eventLocation,
-            String eventPointOfContact, double eventCost, int totalParticipants, int totalEventDays,
-            boolean breakfastRequired, boolean lunchRequired, boolean dinnerRequired, double brefakfastCost,
-            double lunchCost, double dinnerCost) {
-        super(eventID, eventName, eventLocation, eventPointOfContact, eventCost, totalParticipants, totalEventDays);
-        this.breakfastRequired = breakfastRequired;
-        this.lunchRequired = lunchRequired;
-        this.dinnerRequired = dinnerRequired;
-        this.breakfastCost = brefakfastCost;
-        this.lunchCost = lunchCost;
-        this.dinnerCost = dinnerCost;
-        
-    }
+    private double conferenceEventCost;
 
-    public boolean isBreakfastRequired() {
-        return breakfastRequired;
-    }
-
-
-    public void setBreakfastRequired(boolean breakfastRequired) {
-        this.breakfastRequired = breakfastRequired;
-    }
-
-
-    public boolean isLunchRequired() {
-        return lunchRequired;
-    }
-
-
-    public void setLunchRequired(boolean lunchRequired) {
-        this.lunchRequired = lunchRequired;
-    }
-
-
-    public boolean isDinnerRequired() {
-        return dinnerRequired;
-    }
-
-
-    public void setDinnerRequired(boolean dinnerRequired) {
-        this.dinnerRequired = dinnerRequired;
-    }
-
-
-    public double getBrefakfastCost() {
+    public double getBreakfastCost(){
         return breakfastCost;
     }
 
-
-    public void setBrefakfastCost(double brefakfastCost) {
-        this.breakfastCost = brefakfastCost;
+    public void setBreakfastCost(double breakfastCost){
+        this.breakfastCost = breakfastCost;
     }
 
-
-    public double getLunchCost() {
+    public double getLunchCost(){
         return lunchCost;
     }
 
-
-    public void setLunchCost(double lunchCost) {
+    public void setLunchCost(double lunchCost){
         this.lunchCost = lunchCost;
     }
 
-
-    public double getDinnerCost() {
+    public double getDinnerCost(){
         return dinnerCost;
     }
 
-    
-
-    public void setDinnerCost(double dinnerCost) {
+    public ConferenceEvent(String eventID, String eventName, String eventLocation, String pointOfContact, int totalParticipants, int totalEventDays, 
+    double breakfastCost, double lunchCost, double dinnerCost){
+        super(eventID, eventName, eventLocation, pointOfContact, totalParticipants, totalEventDays);
+        this.breakfastCost = breakfastCost;
+        this.lunchCost = lunchCost;
         this.dinnerCost = dinnerCost;
     }
-    
-
-
-    public double getConferenceEventcost() {
-        return ConferenceEventcost;
-    }
-
-
-    public void setConferenceEventcost(double conferenceEventcost) {
-        ConferenceEventcost = conferenceEventcost;
-    }
-    @Override
-    public double calculateEventCost(){
-        double ConferenceEventcost = super.getEventCost();
-        if(breakfastRequired)
-        {
-            ConferenceEventcost=breakfastCost*super.getTotalParticipants()+ConferenceEventcost;
-        }else if (lunchRequired) {
-            ConferenceEventcost=lunchCost*super.getTotalParticipants()+ConferenceEventcost;
-        }
-        else if(dinnerRequired)
-        {
-            ConferenceEventcost=lunchCost*super.getTotalParticipants()+ConferenceEventcost;
-        }  
-        return ConferenceEventcost;
-    }
-
 
 
     @Override
-    public String toString() {
-        return "ConferenceEvent [breakfastRequired=" + breakfastRequired + ", lunchRequired=" + lunchRequired
-                + ", dinnerRequired=" + dinnerRequired + ", brefakfastCost=" + breakfastCost + ", lunchCost="
-                + lunchCost + ", dinnerCost=" + dinnerCost + ", ConferenceEventcost=" + ConferenceEventcost + "]";
+    public void calculateEventCost(){
+        conferenceEventCost = getEventCost() + ((breakfastCost + lunchCost + dinnerCost) * getTotalParticipants() * getTotalEventDays());
     }
 
+
+    @Override
+    public String toString(){
+        return "Conference Event details: " + "\n" +
+        "Event ID: " + getEventID() + "\n" +
+        "Event Name: " + getEventName() + "\n" +
+        "Event Location: " + getEventLocation() + "\n" +
+        "Total participants: " + getTotalParticipants() + "\n" +
+        "Total Conference Cost: " + conferenceEventCost;
+    } 
+        
 }
+
+    
--- interitance/src/Event.java
+++ interitance/src/Event.java
@@ -1,70 +1,93 @@
-public abstract class  Event {
+
+
+public  class Event{
+
+    public static final double insuranceCost = 1000.00;
+    public static final double baseEventCost= 5000;
+    public static final double tax = 0.3;
+
     private String eventID;
     private String eventName;
     private String eventLocation;
-    private String eventPointOfContact;
+    private String pointOfContact;
     private double eventCost;
+    
     private int totalParticipants;
     private int totalEventDays;
-    
 
     public Event(){}
-    public Event(String eventID, String eventName, String eventLocation, String eventPointOfContact, double eventCost,
-            int totalParticipants, int totalEventDays) {
+    
+    public Event(String eventID, String eventName, String eventLocation, String pointOfContact, int totalParticipants, int totalEventDays){
         this.eventID = eventID;
         this.eventName = eventName;
         this.eventLocation = eventLocation;
-        this.eventPointOfContact = eventPointOfContact;
-        this.eventCost = eventCost;
+        this.pointOfContact = pointOfContact;
         this.totalParticipants = totalParticipants;
         this.totalEventDays = totalEventDays;
+
     }
-    public String getEventID() {
+
+    public String getEventID(){
         return eventID;
     }
-    public void setEventID(String eventID) {
+
+    public void setEventID(String eventID){
         this.eventID = eventID;
     }
-    public String getEventName() {
+
+    public String getEventName(){
         return eventName;
     }
-    public void setEventName(String eventName) {
+
+    public void setEventName(String eventName){
         this.eventName = eventName;
     }
-    public String getEventLocation() {
+
+    public String getEventLocation(){
         return eventLocation;
     }
-    public void setEventLocation(String eventLocation) {
-        this.eventLocation = eventLocation;
+
+    public void setEventLocation(String location){
+        this.eventLocation = location;
     }
-    public String getEventPointOfContact() {
-        return eventPointOfContact;
+
+    public String getPointOfContact(){
+        return pointOfContact;
     }
-    public void setEventPointOfContact(String eventPointOfContact) {
-        this.eventPointOfContact = eventPointOfContact;
+
+    public void setPointOfContact(String pointOfContact){
+        this.pointOfContact = pointOfContact;
     }
-    public double getEventCost() {
+
+    /* public double getEventCost(){
         return eventCost;
     }
-    public void setEventCost(double eventCost) {
+
+ */    /* public void setEventCost(double eventCost){
         this.eventCost = eventCost;
-    }
-    public int getTotalParticipants() {
+    } */
+
+    public int getTotalParticipants(){
         return totalParticipants;
     }
-    public void setTotalParticipants(int totalParticipants) {
+
+    public void setTotalParticipants(int totalParticipants){
         this.totalParticipants = totalParticipants;
     }
-    public int getTotalEventDays() {
+
+    public int getTotalEventDays(){
         return totalEventDays;
     }
-    public void setTotalEventDays(int totalEventDays) {
+
+    public double getEventCost(){
+        return eventCost;
+    }
+
+    public void setTotalEventDays(int totalEventDays){
         this.totalEventDays = totalEventDays;
     }
 
-    public abstract double calculateEventCost();
+    
 
-    @Override
-    public abstract String toString();
-
+    public  void calculateEventCost(){}
 }
